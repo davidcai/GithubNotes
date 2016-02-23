a8a0fbbd1acf419fc50270afba7bee28455d1afe
Akila Balasubramanian <abalasubramanian@financialengines.com>
2016-02-23 11:25:46 -0800

    Fix URL.

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,11 @@
 const api = {
 
   getBio(username) {
-    username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}`;
-    return fetch(url).then((res) => res.json());
+    username = username.toLowercase().trim();
+    var url = 'https://api.github.com/users/${username}';
+    fetch(url)
+    .then(res => res.json());
   }
 };
-
 
 export default api;
